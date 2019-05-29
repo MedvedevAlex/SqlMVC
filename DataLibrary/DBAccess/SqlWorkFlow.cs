a477441701ef4bf3cf8ac6567a5f13cb3f70bc2c
Alex Medvedev <localisseat@gmail.com>
2019-05-29 19:24:52 +0700

    return results
    return sql results

---
+++ DataLibrary/DBAccess/SqlWorkFlow.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.DBAccess
+{
+    public class SqlWorkFlow
+    {
+        public static string getStringConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["desktop-opmfhdp"].ConnectionString;
+        }
+
+        public static List<T> loadData<T>(string sql)
+        {
+            using (IDbConnection conn = new SqlConnection(getStringConnection()))
+            {
+                return conn.Query<T>(sql).ToList();
+            }
+        }
+        public static void saveData<T>(string sql, T data)
+        {
+            using (IDbConnection conn = new SqlConnection(getStringConnection()))
+            {
+                conn.Execute(sql, data);
+            }
+        }
+    }
+}
